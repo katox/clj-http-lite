ed3dae784e3735921cf38ce51322f9d5af616269
Kamil Toman <kamil.toman@leafclick.com>
2013-11-29 16:30:06 +0100

    create lf1 release

--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
-(defproject clj-http-lite "0.2.1-SNAPSHOT"
+(defproject katox/clj-http-lite "0.2.1-LF1"
   :description "A Clojure HTTP library similar to clj-http, but more lightweight."
   :url "https://github.com/hiredman/clj-http-lite/"
-  :repositories {"sona" "http://oss.sonatype.org/content/repositories/snapshots"}
   :warn-on-reflection false
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [slingshot "0.10.3"]]
-  :profiles {:dev {:dependencies [[ring/ring-jetty-adapter "1.0.2"]
-                                  [ring/ring-devel "1.0.2"]
+  :profiles {:dev {:dependencies [[ring/ring-jetty-adapter "1.2.1"]
+                                  [ring/ring-devel "1.2.1"]
                                   [lein-multi "1.1.0"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-alpha5"]]}}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :dev-dependencies [[ring/ring-jetty-adapter "1.0.2"]
                      [ring/ring-devel "1.0.2"]
                      [lein-multi "1.1.0"]]
